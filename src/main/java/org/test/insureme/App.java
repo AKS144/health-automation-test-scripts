19493ece6e99ebf5cc8408fdeac67ce2f0256752
AKS144 <32642839+AKS144@users.noreply.github.com>
2023-11-24 23:19:27 +0530

    Update App.java

--- src/main/java/org/test/insureme/App.java
+++ src/main/java/org/test/insureme/App.java
@@ -2,11 +2,11 @@ package org.test.insureme;
 
 import java.io.File;
 import java.io.IOException;
-import java.sql.DriverManager;
+//import java.sql.DriverManager;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.io.FileUtils;
-import org.apache.hc.core5.util.Asserts;
+//import org.apache.hc.core5.util.Asserts;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
@@ -28,8 +28,7 @@ public class App
     System.out.println("Script Started");	
        //initializing the web driver
    // System.setProperty("webdriver.chrome.driver", "/Users/shubham/Documents/softwares/chrome-driver/chromedriver");
-       
-         //System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+    System.setProperty("webdriver.chrome.driver", "C:/Users/lenovo/eclipse-workspace/chromedriver/chromedriver.exe");
     WebDriverManager.chromedriver().setup();
     //setting properties
     ChromeOptions chromeOptions = new ChromeOptions();
@@ -37,7 +36,7 @@ public class App
     // open url
     System.out.println("Driver opening up the url in browser");	
     WebDriver driver = new ChromeDriver(chromeOptions);
-    driver.get("http://15.207.14.223:8081/contact.html");	
+    driver.get("http://15.207.14.223:8081//contact.html");	
     
     //invole implicit waits to load the page
     driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
@@ -45,9 +44,9 @@ public class App
     System.out.println("Enter details in the form");
     //enter details
     //input name
-    driver.findElement(By.id("inputName")).sendKeys("Abhishek");
+    driver.findElement(By.id("inputName")).sendKeys("Sam");
     Thread.sleep(1000);
-    driver.findElement(By.id("inputNumber")).sendKeys("9953436699");
+    driver.findElement(By.id("inputNumber")).sendKeys("999999999");
     Thread.sleep(1000);
     driver.findElement(By.id("inputMail")).sendKeys("sam@xyz.com");
     Thread.sleep(1000);
@@ -66,20 +65,20 @@ public class App
     
     File srcFile = scrShot.getScreenshotAs(OutputType.FILE);
     
-  //  File destFile = new File("/Users/shubham/Documents/test-reports.jpg");
-    File destFile = new File("/var/lib/jenkins/workspace/insure-me-test-scripts/test-reports.jpg");
+    File destFile = new File("/Users/lenovo/Documents/test-reports.jpg");
+    //File destFile = new File("/var/lib/jenkins/workspace/insure-me-test-scripts/test-reports.jpg");
     
     
     FileUtils.copyFile(srcFile, destFile);
     
     Thread.sleep(1000);
-     
+    
+ 
    
     driver.quit();
     
+       	
     
-    	
-    	
     	
         System.out.println( "Hello World!" );
     }
